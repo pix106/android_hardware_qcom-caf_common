46db4da0cbdca0400187c8c0f790edf1b60ebb38
Immanuel Raj <iamimmanuelraj@gmail.com>
2023-06-24 23:19:31 +0530

    BoardConfigQcom: Allow sdm660 to enable and use DRM_PP
    SDM660 Has support in hal
    
    Thanks to @CHRISL7 for clarification
    
    Change-Id: Idbc5f374a2ae4ef9ddb3b64352d35b9f973d2b0a

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -275,7 +275,7 @@ endif
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
 # Enable DRM PP driver on UM platforms that support it
-ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY) $(UM_6_1_FAMILY) $(UM_6_6_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY) $(UM_6_1_FAMILY) $(UM_6_6_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     SOONG_CONFIG_qtidisplay_drmpp := true
 endif
 
