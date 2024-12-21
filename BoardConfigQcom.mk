b43b186c1e3029e9191fb35dba58f2acd2a76515
Skyblueborb <tomaszborbely0710@gmail.com>
2024-12-21 16:31:45 +0100

    common: add soong config for RAW10 buffer fix
    Change-Id: Idea303d58860152541d1d2a2b98f46e716cc67a6
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -192,7 +192,8 @@ SOONG_CONFIG_qtidisplay += \
     target_uses_unaligned_nv21_zsl \
     target_uses_unaligned_ycrcb \
     target_uses_ycrcb_camera_preview \
-    target_uses_ycrcb_venus_camera_preview
+    target_uses_ycrcb_venus_camera_preview \
+    target_needs_raw10_buffer_fix
 
 # Set default values for qtidisplay config
 SOONG_CONFIG_qtidisplay_composer_version ?= v3_3
@@ -223,6 +224,7 @@ SOONG_CONFIG_qtidisplay_target_uses_unaligned_nv21_zsl ?= false
 SOONG_CONFIG_qtidisplay_target_uses_unaligned_ycrcb ?= false
 SOONG_CONFIG_qtidisplay_target_uses_ycrcb_camera_preview ?= false
 SOONG_CONFIG_qtidisplay_target_uses_ycrcb_venus_camera_preview ?= false
+SOONG_CONFIG_qtidisplay_target_needs_raw10_buffer_fix ?= false
 
 ifneq ($(TARGET_DISPLAY_SHIFT_HORIZONTAL),)
     SOONG_CONFIG_qtidisplay_shift_horizontal := $(TARGET_DISPLAY_SHIFT_HORIZONTAL)
@@ -269,6 +271,10 @@ ifeq ($(TARGET_USES_YCRCB_CAMERA_PREVIEW),true)
     SOONG_CONFIG_qtidisplay_target_uses_ycrcb_camera_preview := true
 else ifeq ($(TARGET_USES_YCRCB_VENUS_CAMERA_PREVIEW),true)
     SOONG_CONFIG_qtidisplay_target_uses_ycrcb_venus_camera_preview := true
+endif
+
+ifeq ($(TARGET_NEEDS_RAW10_BUFFER_FIX),true)
+    SOONG_CONFIG_qtidisplay_target_needs_raw10_buffer_fix := true
 endif
 
 # Tell HALs that we're compiling an AOSP build with an in-line kernel
